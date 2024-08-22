70847a343d55799675f2041585e5b0cb1b54d0da
Lucas Stein <stein.lucas2001@gmail.com>
2024-08-22 16:07:27 -0400

    Displays items
    Now shows stored items.

--- script.js
+++ script.js
@@ -7,6 +7,26 @@ document.querySelector("#enter").addEventListener("click", () => {
     createItem(item)
 })
 
+function displayItems(){
+    let items = ""
+    for(let i = 0; i < itemsArray.length; i++){
+        items += `  <div class="item">
+                        <div class="input-contorller">
+                            <textarea disabled>${itemsArray[i]}</textarea>
+                            <div class="edit-controller">
+                                <i class="fa-solid fa-check deleteBtn"></i>
+                                <i class="fa-solid fa-pen-to-square editBtn"></i>
+                            </div>
+                        </div>
+                        <div class="update-controller">
+                            <button class="saveBtn">Save</button>
+                            <button class="cancelBtn">Cancel</button>
+                        </div>
+                    </div>`
+    }
+    document.querySelector(".to-do-list").innerHTML = items
+}
+
 function createItem(item){
     itemsArray.push(item.value)
     localStorage.setItem("items", JSON.stringify(itemsArray))
@@ -21,4 +41,5 @@ function displayDate(){
 
 window.onload = function(){
     displayDate()
+    displayItems()
 }
